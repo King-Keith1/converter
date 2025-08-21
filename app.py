6f42dca66a8c84ee5cae039ded2ffb35664f6419
King-Keith1 <keith4321ben@gmail.com>
2025-08-21 16:17:44 +0200

    commit

--- app.py
+++ app.py
@@ -2,21 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-@app.route('/world-clock')
-def world_clock():
-    return render_template('world_clock.html')
-
-@app.route('/currency-converter')
-def currency_converter():
-    return render_template('currency_converter.html')
-
-@app.route('/pinned-items')
-def pinned_items():
-    return render_template('pinned_items.html')
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
